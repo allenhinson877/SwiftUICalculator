c3e372407dc3f57e1cebb20da2f2c27e9915d728
allenhinson877 <58499378+allenhinson877@users.noreply.github.com>
2022-03-14 13:51:47 -0500

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by William Hinson on 3/14/22.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
